98a4ebf38fe04308f94f6245b6eb48788cd1ed44
leehyeji <leehyeji95@gmail.com>
2022-03-20 23:02:34 +0900

    Spring Security OAuth API 220320 빌드 완성본 2차

--- backend/src/main/java/com/studygram/controller/AuthController.java
+++ backend/src/main/java/com/studygram/controller/AuthController.java
@@ -25,8 +25,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.util.Date;
 
 @RestController
-//@RequestMapping("/api/v1/auth")
-@RequestMapping("auth")
+@RequestMapping("/api/v1/auth")
 @RequiredArgsConstructor
 public class AuthController {
 
