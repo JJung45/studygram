65e6889d6bfd4463ba56728939910ef477198e76
JJung45 <753cindy@naver.com>
2022-07-27 21:04:50 +0900

    react 게시물 저장

---
+++ frontend/src/container/main/SavePostContainer.js
@@ -0,0 +1,8 @@
+import React, { useEffect } from "react";
+import SavePostComponent from "../../component/main/SavePostComponent";
+
+const SavePostContainer = () => {
+  return <SavePostComponent />;
+};
+
+export default SavePostContainer;
