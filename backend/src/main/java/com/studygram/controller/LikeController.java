7b0a878cc62b2f16cde27fa15c9de10dc683c8f4
minchoi <53928609+minchoi9509@users.noreply.github.com>
2022-10-18 22:45:11 +0900

    좋아요 추가,삭제

--- backend/src/main/java/com/studygram/controller/LikeController.java
+++ backend/src/main/java/com/studygram/controller/LikeController.java
@@ -10,23 +10,21 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/like")
+@RequestMapping("/api/like")
 public class LikeController {
 
     @Autowired
     private LikeService likeService;
 
     @PostMapping(path="/save")
-    public ApiResponse addLike(@RequestBody Like like) throws Exception {
+    public ApiResponse save(@RequestBody Like like) throws Exception {
         likeService.save(like);
         return ApiResponse.success(HttpStatus.OK.name(), like);
     }
 
     @DeleteMapping(path="/{postId}")
-    public void deleteLike(@PathVariable(name="postId") int postId) throws Exception {
-        // TODO Session get userId
-        Like like = likeService.findByPostUser(postId, 28);
-        likeService.delete(like);
+    public void cancle(@PathVariable(name="postId") int postId) {
+        likeService.delete(postId);
     }
 
 }
