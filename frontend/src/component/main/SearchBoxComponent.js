8d7277c2aadb4d12e583b257ce48e296963bcc8a
leehyeji <leehyeji95@gmail.com>
2023-03-26 21:47:23 +0900

    검색 리스트 수정중

--- frontend/src/component/main/SearchBoxComponent.js
+++ frontend/src/component/main/SearchBoxComponent.js
@@ -1,5 +1,6 @@
 import React, {useState, useEffect} from 'react';
 import searchAPI from "../../lib/api/search"
+import {Link} from "react-router-dom";
 
 const SearchBoxComponent = () => {
     const [keyword, setKeyword] = useState('');
@@ -8,15 +9,25 @@ const SearchBoxComponent = () => {
     const onKeywordChange = (e) => {
         console.log('검색', keyword);
         setKeyword(e.target.value);
+        if(keyword.length === 0) {
+            setSearchList([]);
+        }
+
     };
 
     useEffect(() => {
-        // searchKeyword(keyword, 1);
         console.log(keyword);
-        searchAPI.search(keyword, 1)
-            .then((res) => {
-                setSearchList(res.data);
-            });
+        if(keyword.length === 0) {
+            setSearchList([]);
+        }
+        else {
+            searchAPI.search(keyword, 2)
+                .then((res) => {
+                    console.log('검색결과: ', res.data.body.searchList);
+                    const resultList = res.data.body.searchList;
+                    setSearchList(resultList);
+                });
+        }
     }, [keyword]);
 
     return (
@@ -29,9 +40,28 @@ const SearchBoxComponent = () => {
             onChange={onKeywordChange}
             value={keyword}
             />
-            <ul>
-                {/*{searchList.map((row, index) => <li key={index}>{row.username}</li>)}*/}
-            </ul>
+            {searchList.map((item, idx) => {
+                return (
+                    <>
+                        <div className="search-result" key={idx + item.idx}>
+                            <Link to={"/" + item.userName}>
+                                {/*뒤로가기 시 자동완성 남아있는 경우 비워주는 코드*/}
+                                <div className="recommend-friend-profile" onClick={() => setSearchList([])}>
+                                    <img
+                                        className="img-profile"
+                                        src={item.profileImageUrl}
+                                        alt="프로필 사진"
+                                    />
+                                    <div className="profile-text">
+                                        <span className="userID point-span">{item.userName}</span>
+                                    </div>
+                                </div>
+                            </Link>
+
+                        </div>
+                    </>
+                )
+            })}
         </div>
     )
 };
