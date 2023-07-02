d8f6fca6661aa11b2c77937edb920082bdac0aa7
leehyeji <leehyeji95@gmail.com>
2023-07-02 22:39:02 +0900

    post 수정

--- frontend/src/component/main/Post.js
+++ frontend/src/component/main/Post.js
@@ -1,5 +1,5 @@
-import React, { useState,useEffect } from "react";
-import { Link } from "react-router-dom";
+import React, {useState, useEffect, useRef} from "react";
+import {Link} from "react-router-dom";
 
 import PostApi from "../../lib/api/post";
 import LikeApi from "../../lib/api/like";
@@ -11,17 +11,16 @@ import {convertContentTag} from "../../module/utils/convertContentTag";
 import moment from "moment";
 import PostModal from "../auth/PostModal";
 
-const Post = ({ data }) => {
-  const [post, setPost] = useState(data);
-  const [modalOpen, setModalOpen] = useState(false);
-  const [likers, setLikers] = useState(null);
-  const [comments, setComments] = useState(null);
-  const [comment, setComment] = useState({
-    content: "",
-  });
-  const [commentOpen,setCommentOpen] = useState(false);
-  const [postModalOpen, setPostModalOpen] = useState(false);
-  const [selectedPost, setSelectedPost] = useState(null);
+const Post = ({data}) => {
+    const [post, setPost] = useState(data);
+    const [modalOpen, setModalOpen] = useState(false);
+    const [likers, setLikers] = useState(null);
+    const [comment, setComment] = useState({
+        postIdx: post.idx,
+        content: "",
+    });
+    const [postModalOpen, setPostModalOpen] = useState(false);
+    const [selectedPost, setSelectedPost] = useState(null);
 
     const deleteLike = (data) => {
         const postIdx = data.idx;
@@ -74,42 +73,43 @@ const Post = ({ data }) => {
         setSelectedPost(post);
     };
     const closePostModal = () => {
-        setPost(false);
+        setPostModalOpen(false);
         setSelectedPost(null);
     };
 
     const handleCommentChange = (e) => {
         const {name, value} = e.target;
-        setComment(value);
+        setComment({
+            ...comment,
+            [name]: value,
+        });
     }
     const handleCommentSubmit = (e) => {
         e.preventDefault();
-        if (comment === "") {
+        if (comment.content === "") {
             alert("내용을 입력하세요");
             return;
         }
-        CommentApi.addComment({
-            postIdx: post.idx,
-            content: comment,
-        }).then(() => reloadPost(post.idx));
+        CommentApi.addComment(comment)
+            .then((res) => {
+                reloadPost(post.idx);
+                setComment({
+                    postIdx: post.idx,
+                    content: "",
+                })
+            });
     }
 
     const getTimeDifference = (post) => {
         const diffInMinutes = moment().diff(moment(post.createdDate), "minutes");
         const diffInHours = moment().diff(moment(post.createdDate), "hours");
         const diffInDays = moment().diff(moment(post.createdDate), "days");
-        const humanizedTimeDiff =
-            diffInMinutes < 60
-                ? `${diffInMinutes}분 전`
-                : diffInHours >= 24
-                    ? `${diffInDays}일 전`
-                    : `${diffInHours}시간 전`;
+        const humanizedTimeDiff = diffInMinutes < 60 ? `${diffInMinutes}분 전` : diffInHours >= 24 ? `${diffInDays}일 전` : `${diffInHours}시간 전`;
 
         return humanizedTimeDiff;
     }
 
-    return (
-        <article>
+    return (<article>
             <header>
                 <div className="profile-of-article">
                     <img
@@ -136,24 +136,20 @@ const Post = ({ data }) => {
             </div>
             <div className="icons-react">
                 <div className="icons-left">
-                    {!post.hasLiked && (
-                        <img
+                    {!post.hasLiked && (<img
                             className="icon-react"
                             src="https://s3.ap-northeast-2.amazonaws.com/cdn.wecode.co.kr/bearu/heart.png"
                             alt="heart_disLike"
                             onClick={() => {
                                 saveLike(post);
                             }}
-                        />
-                    )}
-                    {post.hasLiked && (
-                        <i
+                        />)}
+                    {post.hasLiked && (<i
                             className="ri-heart-3-fill ri-admin-line ri-xl"
                             onClick={() => {
                                 deleteLike(post);
                             }}
-                        ></i>
-                    )}
+                        ></i>)}
                     <img
                         className="icon-react"
                         src="https://s3.ap-northeast-2.amazonaws.com/cdn.wecode.co.kr/bearu/comment.png"
@@ -202,13 +198,11 @@ const Post = ({ data }) => {
                         {convertContentTag(data.content, data.tags)}
                     </p>
                 </div>
-                {data.commentCnt !== 0 && (
-                    <div className="comment-section">
+                {post.commentCnt !== 0 && (<div className="comment-section">
                         <Link onClick={() => openPostModal(post)}>
-                            댓글 {data.commentCnt}개 모두 보기
+                            댓글 {post.commentCnt}개 모두 보기
                         </Link>
-                    </div>
-                )}
+                    </div>)}
                 <form onSubmit={handleCommentSubmit} method="post">
                     <input
                         id="postIdx"
@@ -221,7 +215,7 @@ const Post = ({ data }) => {
                         name="content"
                         type="text"
                         placeholder="댓글 달기"
-                        value={comment}
+                        value={comment.content}
                         onChange={handleCommentChange}
                     />
                     <button type="submit">게시</button>
@@ -236,8 +230,7 @@ const Post = ({ data }) => {
                 close={closePostModal}
                 post={selectedPost}>
             </PostModal>
-        </article>
-    );
+        </article>);
 };
 
 export default Post;
