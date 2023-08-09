69e079db16f0f05fad70b87b61f37d4924a35f06
JJung45 <753cindy@naver.com>
2023-08-10 00:45:08 +0900

    comment time diff 추가 및 username 추가

--- frontend/src/component/main/PostCommentComponent.js
+++ frontend/src/component/main/PostCommentComponent.js
@@ -1,10 +1,23 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import CommentApi from "../../lib/api/comment";
+import moment from "moment";
 
 const Comment = ({ data }) => {
-  console.log(data);
-   /** userName이 안나옴.. */
+
+  const getTimeDifference = (data) => {
+    const diffInMinutes =  moment().diff(moment(data.createdDate), "minutes");
+    const diffInHours =  moment().diff(moment(data.createdDate), "hours");
+    const diffInDays =  moment().diff(moment(data.createdDate), "days");
+    const humanizedTimeDiff =
+      diffInMinutes < 60
+        ? `${diffInMinutes}분 전`
+        : diffInHours >= 24
+        ? `${diffInDays}일 전`
+        : `${diffInHours}시간 전`;
+  
+    return humanizedTimeDiff;
+  }
 
   return (
     <div className="content">
@@ -12,12 +25,15 @@ const Comment = ({ data }) => {
         <img src={data.profileImageUrl} alt="프로필이미지" />
       </div>
       <div className="posting">
-        <span className="userID point-span">{data.userIdx}</span>
+        <span className="userID point-span">{data.userName}</span>
         <div className="post-content"> {data.content}</div>
       </div>
       <span className="heart">
         ♡
       </span>
+      <div className="time">
+        { getTimeDifference(data) }
+      </div>
     </div>
   );
 };
