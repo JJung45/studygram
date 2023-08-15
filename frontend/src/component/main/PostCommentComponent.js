ccf38ff17ac8553d6fc4569742483578ee25e356
leehyeji95 <leehyeji95@gmail.com>
2023-08-15 16:28:53 +0900

    프로필 사진 변경 수정진행중

--- frontend/src/component/main/PostCommentComponent.js
+++ frontend/src/component/main/PostCommentComponent.js
@@ -1,11 +1,10 @@
 import React from "react";
 import { Link } from "react-router-dom";
 const Comment = ({ data }) => {
-  console.log(data); //TODO findCommentsByPostIdx 추가정보 가져오기
   return (
     <div className="content">
       <div className="image">
-        <img src="" alt="프로필이미지" />
+        <img src={data.profileImageUrl} alt="프로필이미지" />
       </div>
       <div className="posting">
         <span className="userID point-span">{data.userId}</span>
