e9f8f6085472d56775ee9f84bbcf30147c2c4e55
JJung45 <753cindy@naver.com>
2023-08-22 20:57:18 +0900

    likeService 노티 설정 수정

--- backend/src/main/java/com/studygram/service/LikeService.java
+++ backend/src/main/java/com/studygram/service/LikeService.java
@@ -2,10 +2,14 @@ package com.studygram.service;
 
 import com.studygram.domain.Like;
 import com.studygram.domain.NotificationType;
+import com.studygram.domain.Post;
 import com.studygram.domain.User;
 import com.studygram.mapper.LikeMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -20,11 +24,21 @@ public class LikeService {
 
     private final NotificationService notificationService;
 
+    private final PostService postService;
+
     public void save(Like like) {
-        User user = userService.getUser();
+        // UserID 가져오기
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+        User user = userService.getUser(userDetails.getUsername());
+
         like.setUserIdx(user.getIdx());
         likeMapper.save(like);
-        notificationService.send(like.getPostUserIdx(), user.getIdx(), like.getIdx(), NotificationType.LIKE);
+
+        Post post = postService.findById(like.getPostIdx());
+        int toUserIdx = post.getUserIdx();
+
+        notificationService.send(toUserIdx, user.getIdx(), like.getIdx(), NotificationType.LIKE);
     }
 
     public int countAll() {
