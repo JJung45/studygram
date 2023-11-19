90e0d1a9045ff12f2e0b9ba3c13700ec1e451fbc
JJung45 <753cindy@naver.com>
2023-11-19 21:35:20 +0900

    css 오류 수정

--- frontend/src/component/main/Post.js
+++ frontend/src/component/main/Post.js
@@ -217,16 +217,6 @@ const Post = ({ data }) => {
             >
               {likeMessage(post)}
             </span>
-            <p>
-              {convertContentTag(data.content, data.tags)}
-            </p>
-          </div>
-          <div className="comments">
-            {post.commentCnt !== 0 && (<div className="comment-section">
-                            <Link onClick={() => openPostModal(post)}>
-                                댓글 {post.commentCnt}개 모두 보기
-                            </Link>
-                        </div>)}
           </div>
         </div>
       )}
